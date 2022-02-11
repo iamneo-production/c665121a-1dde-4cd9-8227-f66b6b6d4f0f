775ebccf68e3fd58b48cb842d4e708badc9fd1c4
SUBHANKAR MAJI <subhankarmaji500@gmail.com>
2022-02-11 10:35:51 +0530

    frontend of homepage,register,login

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import Navbar from './components/screens/Navbar'
+import Register from './components/screens/Register'
+import Login from './components/screens/Login'
+import Mainpage from './components/screens/Mainpage'
+import {
+  BrowserRouter as Router,Switch,Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div>
+    <Router>
+      <Switch>
+      <Route path="/" exact component={Mainpage}></Route>
+        <Route path="/Login" component={Login}></Route>
+        <Route path="/Register" exact component={Register}></Route>
+      </Switch>
+    </Router>
+  </div>
+    
   );
 }
 
