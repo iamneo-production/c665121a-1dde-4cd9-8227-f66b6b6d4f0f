431b254930119ceb12e81b95dde3ddb6428ee130
Nandhakumar <nandhakumar.baskar2000@gmail.com>
2022-02-21 14:17:10 +0530

    Apointments booking slots file added

---
+++ reactapp/src/components/screens/data.js
@@ -0,0 +1,16 @@
+const data  = {
+    service_center:[
+        {
+          
+            serviceCenterName: 'Fastrack Service',
+            serviceCenterPhone: '8767896543',
+            serviceCenterAddress: 'Chennai',
+            serviceCenterImageUrl: 'https://m.media-amazon.com/images/I/717uqMkUwNL._UY445_.jpg',
+            serviceCentermailId: 'fastrack@gmail.com',
+            serviceCenterDescription: 'fastrack service'
+        },
+        
+    ]
+}
+
+export default data;
