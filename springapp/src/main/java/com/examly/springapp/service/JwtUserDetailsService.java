1db81e94652d48225031f8df5332a819542a5da4
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-02-28 16:31:53 +0530

    Create JwtUserDetailsService.java

---
+++ springapp/src/main/java/com/examly/springapp/service/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.dao.UserDao;
+import com.examly.springapp.entity.Users;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Users user = userDao.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+	}
+	
+	public Users save(Users user) {
+		user.setPassword(bcryptEncoder.encode(user.getPassword()));
+		user.setRole("user");
+		return userDao.save(user);
+	}
+}
