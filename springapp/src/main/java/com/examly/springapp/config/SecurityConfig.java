4b1ae130af84877c2d7f13161d994643eee6b823
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-03-05 16:58:52 +0530

    Update SecurityConfig.java

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
     configuration.setAllowedHeaders(Collections.singletonList("*"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
