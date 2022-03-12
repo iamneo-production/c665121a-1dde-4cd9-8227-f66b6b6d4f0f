32b7cfcdbd80b6411d769f71ce11bbba608c1ba2
MAHALAKSHMI-MUTHUKRISHNAN <maha18124.it@rmkec.ac.in>
2022-03-12 14:00:20 +0530

    Protected Routes

--- reactapp/src/components/screens/LoginForm.js
+++ reactapp/src/components/screens/LoginForm.js
@@ -7,34 +7,37 @@ import axiosObject from '../../api/bootapi';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function LoginForm(){
-  // const validate = Yup.object({
+  const validate = Yup.object({
     
-  //   email: Yup.string()
-  //     .email('Email is invalid')
-  //     .required('Email is required'),
-  //   password: Yup.string()
+    username: Yup.string()
+      .required('Username is required'),
+    password: Yup.string()
       
-  //     .required('Password is required'),
-  // })
+      .required('Password is required'),
+  })
   const sendData=(data)=>{
     console.log("I'm here");
     axiosObject.post("/authenticate",data).then(res=>{
       console.log(res);
+      localStorage.setItem("isAuthenticated", "true");
       localStorage.setItem("usertoken",res.data.jwtToken);
       axiosObject.get("/mydetails").then(res=>{
         console.log(res.data);
         localStorage.setItem("user",JSON.stringify(res.data));
       })
       if(data.username==="admin") {
+        localStorage.setItem("isAdmin","true");
         toast.success('Welcome Admin',{autoClose: 2000});
         setTimeout(() => { window.location.replace('/admin/home'); }, 2000);
         
       } else {
+        localStorage.setItem("isUser","true");
         toast.success('Welcome User',{autoClose: 2000});
         setTimeout(() => {  window.location.replace('/user/home'); }, 2000);
       }
     }).catch(err=>{
       console.log(err);
+      toast.error('Invalid Credentials',{autoClose: 2000});
     })
   }
   return (
@@ -45,7 +48,7 @@ function LoginForm(){
         username: '',
         password: '',
       }}
-      // validationSchema={validate}
+      validationSchema={validate}
       onSubmit={values => {
         console.log(values);
         sendData(values);
--- reactapp/src/components/screens/Navbar.js
+++ reactapp/src/components/screens/Navbar.js
@@ -6,6 +6,9 @@ function NavBar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const handleLogout = () => {
+    localStorage.clear();
+  }
   return (
     <>
       <nav className="navbar">
@@ -65,7 +68,7 @@ function NavBar() {
                 to="/"
                 activeClassName="active"
                 className="nav-links"
-                onClick={handleClick}
+                onClick={handleLogout}
               >
                 Logout
               </NavLink>
