d6e713c22c08229127d13cc56180c1e5a8386752
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-03-15 16:12:47 +0530

    adding functional automation

---
+++ automation/src/test/java/config/AppConstants.java
@@ -0,0 +1,7 @@
+package config;
+
+public class AppConstants {
+    public static String REPORT_PATH = "/test-output/index.html";
+    public static String SCREEN_SHOT_PATH = "/test-output/screenshots/";
+    public static String BASE_URL = "https://www.google.com/";
+}
