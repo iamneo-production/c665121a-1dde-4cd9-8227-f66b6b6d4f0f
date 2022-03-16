d27501974d48af5a4feb472072378a9fde47e8e5
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-03-16 14:16:59 +0530

    Adding Pages.

--- automation/src/test/java/config/DemoTest.java
+++ automation/src/test/java/config/DemoTest.java
@@ -1,15 +1,27 @@
 package config;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 
 public class DemoTest extends TestConfig{
+  LoginPage loginPage ;
+  @BeforeMethod
+  public void setDriver(){
+    loginPage = new LoginPage(driver);
+  }
 
   @Test
   public void demo(){
     test = extent.createTest("Demo Test");
-    Reporter.log("working");
-    Assert.assertEquals(1,1);
+    try{
+      driver.findElement(By.id("login-main")).click();
+      this.loginPage.login("admin","admin@123");
+    }catch (Exception e) {
+      test.fail(e.toString());
+    }
   }
 }
