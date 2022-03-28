31ef0f93c91f5163c2739a989b903dc78c91ecad
MAHALAKSHMI-MUTHUKRISHNAN <maha18124.it@rmkec.ac.in>
2022-03-28 17:26:10 +0530

    Styling changes

--- reactapp/src/components/screens/ViewSCReview.js
+++ reactapp/src/components/screens/ViewSCReview.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axiosObject from "../../api/bootapi";
 import "../styles/Appointments.css";
 import NavbarUser from "./NavbarUser";
-import CardGroup from "react-bootstrap/CardGroup";
+import '../styles/Home.css';
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -16,13 +16,6 @@ const ViewSCReview = () => {
         star: "3",
         experience: "good",
         givenDate: "2022-03-03",
-      },
-      {
-        center_id: 2,
-        userName: "User2",
-        star: "5",
-        experience: "excellent",
-        givenDate: "2022-04-03",
       }
   ]);
 
@@ -41,26 +34,27 @@ const ViewSCReview = () => {
         }}
       >
         <div className="fixed-content">
-          <Row xs={1} md={4} className="g-4">
+          <Row className="g-4">
             {data.map((val) => {
               return (
-                 <Col style={{ padding: "2rem" }}>
-
-                    <Card style={{backgroundColor:"#EFFFFD",borderRadius:"10%",boxShadow: "7px 7px 7px #9E9E9E",borderColor:"white"}}>
+                
+                <Col style={{ padding: '2rem' }} >
+                
+                <Card style={{ width: '18rem',marginRight:5,marginLeft:5,backgroundColor:"#EFFFFD",borderRadius:"10%",boxShadow: "7px 7px 7px #9E9E9E",borderColor:"white"}}>
                       <div style={{display:'flex',marginLeft:'40%'}}>
                         <Card.Title style={{fontSize:40}} >{val.star}</Card.Title>
                         <Card.Img variant="top" src={star} style={{height:40,width:40}}/>
                       </div>
-                      <Card.Body>
+                  <Card.Body>
                         <Card.Title>{val.userName}</Card.Title>
                         <Card.Text>{val.experience}</Card.Text>
                       </Card.Body>
                       <Card.Footer>
                         <small className="text-muted">{val.givenDate}</small>
                       </Card.Footer>
-                    </Card>
-
-                </Col>
+                </Card>
+               
+              </Col>
               );
             })}
           </Row>
